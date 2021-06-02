3ff63b71b432961873f4b27c409bb1a0fb2f1428
karaage0703 <coldchicken@gmail.com>
2021-06-03 01:09:42 +0900

    Initial commit

---
+++ wfh_monitor_m5.ino
@@ -0,0 +1,59 @@
+#include <M5Stack.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include "time.h"
+#include <BlynkSimpleEsp32.h>
+
+#define BLYNK_PRINT Serial
+const char* auth   = "********";
+const char* ssid     = "********";
+const char* password = "********";
+
+int brightness = 200;
+
+void setup(){
+  M5.begin();
+  // Serial.begin(115200);
+  delay(10);
+
+  // Blink起動
+  Blynk.begin(auth, ssid, password);
+
+  M5.Speaker.begin();
+  M5.Power.begin();
+  M5.Lcd.setRotation(1);
+  M5.Lcd.setBrightness(brightness);
+
+  M5.Lcd.fillScreen(BLUE);
+  M5.Lcd.setTextSize(5);
+  M5.Lcd.setCursor(50, 100);
+  M5.Lcd.setTextColor(BLACK);
+  M5.Lcd.print("OFF AIR\n"); 
+}
+
+void loop(){
+    // Blynk Setting
+    Blynk.run();
+
+    M5.update();
+}
+
+BLYNK_WRITE(V0){
+  int val = param[0].asInt();
+  if(val == 0){
+    M5.Lcd.fillScreen(BLUE);
+    M5.Lcd.setTextSize(5);
+    M5.Lcd.setCursor(50, 100);
+    M5.Lcd.setTextColor(BLACK);
+    M5.Lcd.print("OFF AIR\n"); 
+    delay(1000);    
+  }
+  if(val == 1){
+    M5.Lcd.fillScreen(RED);
+    M5.Lcd.setTextSize(5);
+    M5.Lcd.setCursor(50, 100);
+    M5.Lcd.setTextColor(WHITE);
+    M5.Lcd.print("ON AIR\n"); 
+    delay(1000);
+  }
+}
